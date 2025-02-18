afc8e0c32c7cfe72ad3ecce24cd6aa9736332b7e
Neto test <test@neto.com>
2025-02-19 00:43:39 +0300

    .gitignore without node_modules | without html/css loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-
-const path = require('node:path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   output: {
@@ -15,32 +13,6 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    })
-  ],
 };
